51e321b6855079186ab23654a65edaf2f32445de
mohideenmaheen <syntronismohideen@gmail.com>
2026-02-17 17:13:01 +0530

    update

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,27 +1,20 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const itemRoutes = require("./routes/itemRoutes");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
-
 app.get("/", (req, res) => {
-  res.send("🚀 API Running Successfully");
+  res.send("Server Running 🚀");
 });
-
-app.use("/api/items", itemRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🔥 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
