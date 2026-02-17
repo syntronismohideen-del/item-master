302604ee018a60524db16c755d54a1c41a060051
mohideenmaheen <syntronismohideen@gmail.com>
2026-02-17 17:50:14 +0530

    new status

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,21 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import itemRoutes from "./routes/itemRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-app.use("/api/items", itemRoutes);
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
-  res.send("Server Running 🚀");
+  res.send("🚀 API Running Successfully");
 });
+
+app.use("/api/items", itemRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🔥 Server running on port ${PORT}`);
 });
